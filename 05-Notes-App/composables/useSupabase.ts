5ebef1f35a450d4b7e5c1d4f45c07a2638632faa
Santosh Bhattarai <mr.bullet.sb@gmail.com>
2022-08-14 12:22:58 +0545

    Update useSupabase.ts

--- 05-Notes-App/composables/useSupabase.ts
+++ 05-Notes-App/composables/useSupabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraWJiYmJyaGF4anpsdHNwZHlsIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2MDMxMzgyMiwiZXhwIjoxOTc1ODg5ODIyfQ.mxpn9fYWDaGZFI_9MyK3PDzBQNQGktREJJXmA_xGgq4";
-const SUPABASE_URL = "https://skibbbbrhaxjzltspdyl.supabase.co";
+  "your_supabase_key";
+const SUPABASE_URL = "your_supabase_url";
 
 export function useSupabase() {
   const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
